4e2e25184baf05c7029be9ae42373ee1ecc2167b
andreiandoo <62668614+andreiandoo@users.noreply.github.com>
2026-09-06 16:34:07 +0300

    Add public information catalog rights class

--- app/Enums/CatalogRightsClass.php
+++ app/Enums/CatalogRightsClass.php
@@ -5,6 +5,7 @@ namespace App\Enums;
 enum CatalogRightsClass: string
 {
     case OpenRedistributable = 'open_redistributable';
+    case PublicInformation = 'public_information';
     case PermissionedRedistributable = 'permissioned_redistributable';
     case CommerceOnly = 'commerce_only';
     case InternalReference = 'internal_reference';
