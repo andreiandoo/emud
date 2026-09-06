061e19a40fba5bbc839ce73574fc90b6a3bf5f24
andreiandoo <62668614+andreiandoo@users.noreply.github.com>
2026-09-06 20:48:50 +0300

    Expose catalog source on part relations

--- app/Models/CatalogPartRelation.php
+++ app/Models/CatalogPartRelation.php
@@ -18,4 +18,14 @@ class CatalogPartRelation extends Model
     {
         return $this->belongsTo(CatalogPart::class, 'target_part_id');
     }
+
+    public function source(): BelongsTo
+    {
+        return $this->belongsTo(CatalogSource::class, 'catalog_source_id');
+    }
+
+    public function sourceRecord(): BelongsTo
+    {
+        return $this->belongsTo(CatalogSourceRecord::class, 'catalog_source_record_id');
+    }
 }
